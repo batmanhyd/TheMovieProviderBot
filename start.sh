f1a5ffe6702b4a2a455f11d7896c7de6fa3156e2
batmanhyd <155296058+batmanhyd@users.noreply.github.com>
2024-05-15 16:37:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ] 
 then 
   echo "Cloning main Repository" 
-  git clone https://github.com/Itzmeproman/TheMovieProviderBot.git /DQ-The-File-Donor 
+  git clone https://github.com/batmanhyd/TheMovieProviderBot.git 
 else 
   echo "Cloning Custom Repo from $UPSTREAM_REPO " 
   git clone $UPSTREAM_REPO /DQ-The-File-Donor 
